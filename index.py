78da378afc5a743a07b80898649fb664ac1482fb
smic254 <smich254@gmail.com>
2023-09-19 20:53:50 -0500

    Primer commit

---
+++ index.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from routes.user import user
+from docs import tags_metadata
+
+app = FastAPI(
+  title="FastAPI & Mongo CRUD",
+  description="this is a simple REST API using fastapi and mongodb",
+  version="0.0.1",
+  openapi_tags=tags_metadata
+)
+
+app.include_router(user)
